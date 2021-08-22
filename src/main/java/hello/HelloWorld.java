e765e04dad5b755dd9d4bdf3fc628cea222cb1c0
sridharkondeti <sridharreddy2kx@gmail.com>
2021-08-21 18:58:16 -0600

    Update HelloWorld.java

--- src/main/java/hello/HelloWorld.java
+++ src/main/java/hello/HelloWorld.java
@@ -8,7 +8,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-public class HelloServlet extends HttpServlet {
+public class HelloWorld extends HttpServlet {
         public void doGet(HttpServletRequest request, HttpServletResponse response)
                         throws ServletException, IOException {
                 PrintWriter out = response.getWriter();
