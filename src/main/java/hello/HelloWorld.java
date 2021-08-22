0aa7f2b6d08ea8280934658c3d25dc32ac0111c2
sridharkondeti <sridharreddy2kx@gmail.com>
2021-08-21 19:30:54 -0600

    Update HelloWorld.java

--- src/main/java/hello/HelloWorld.java
+++ src/main/java/hello/HelloWorld.java
@@ -12,6 +12,6 @@ public class HelloWorld extends HttpServlet {
         public void doGet(HttpServletRequest request, HttpServletResponse response)
                         throws ServletException, IOException {
                 PrintWriter out = response.getWriter();
-                out.println("Hello World");
+                out.println("This is my first Hello World Web Application using Tomcat/Servlets !!!");
         }
 }
